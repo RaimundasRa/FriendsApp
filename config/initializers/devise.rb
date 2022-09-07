c291807857e56d972427c546bc416583884f3add
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2022-09-07 15:30:10 +0100

    fixed sign up/in/out as it was not working with turbolinks, fixed button styling for log out button

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,6 +6,19 @@
 # breaking changes in upgrades (i.e., in the event that future versions of
 # Devise change the default values for those options).
 #
+class TurboFailureApp < Devise::FailureApp
+  def respond
+    if request_format == :turbo_stream
+      redirect
+    else
+      super
+    end
+  end
+
+  def skip_format?
+    %w(html turbo_stream */*).include? request_format.to_s
+  end
+end
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
@@ -15,7 +28,14 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '527086b6c9d55927e9891a02cfebc2204277a3deaf2b7de7347daf6581906b39b6034d775a6c7ac37f493a4c18c839363c628953758a5810d76d10227ceb37ba'
+  config.parent_controller = 'TurboDeviseController'
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
 
+  config.warden do |manager|
+    manager.failure_app = TurboFailureApp
+  #   manager.intercept_401 = false
+  #   manager.default_strategies(scope: :user).unshift :some_external_strategy
+  end
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
