3eb03034294cae1e49b2e2ac0f978a96c290b826
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2022-09-15 17:25:30 +0100

    added feature: user only can check his own friend list

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,17 +1,20 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
+  # before_action :correct_user, only: [:update, :destroy]
   def index
-    @friends = Friend.all
+    # @friends = Friend.all
+    @friends = current_user.friends
   end
 
   # GET /friends/1 or /friends/1.json
   def show
   end
 
-  # GET /friends/new
+  # GET /friends/news
   def new
-    @friend = Friend.new
+    # @friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -20,7 +23,8 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    # @friend = Friend.new(friend_params)
+    @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -55,6 +59,10 @@ class FriendsController < ApplicationController
 
     redirect_to friends_path, notice: "Friend was successfully removed from your list.", status: :see_other
   end
+
+# def correct_user
+#   @friend = correct_user.friends.find_by(id: params[:id])
+# end
 
   private
     # Use callbacks to share common setup or constraints between actions.
